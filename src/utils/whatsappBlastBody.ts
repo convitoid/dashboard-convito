df60adecfb4de18f6fd455f337ee0f849f1483de
Convito <convitoid@gmail.com>
2024-08-27 04:07:06 +0000

    change url image whatsapp

--- src/utils/whatsappBlastBody.ts
+++ src/utils/whatsappBlastBody.ts
@@ -92,8 +92,8 @@ export const WhatsappBlastBody = async ({
                         {
                            type: 'image',
                            image: {
-                              //   link: `${process.env.NEXT_BASE_URL}${image.imagePath}`,
-                              link: `https://images.unsplash.com/flagged/photo-1620830102229-9db5c00d4afc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+                                link: `${process.env.NEXTAUTH_URL}${image.imagePath}`,
+                              // link: `https://images.unsplash.com/flagged/photo-1620830102229-9db5c00d4afc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
                            },
                         },
                      ],
